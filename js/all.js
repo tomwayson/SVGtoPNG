3df0e9c9cca6e1d4fb2cbbe76750290516bd8395
Tom Wayson <tom@tomwayson.com>
2015-04-17 20:39:31 -0700

    timeline chart

--- js/all.js
+++ js/all.js
@@ -1,17 +1,30 @@
-
+/* jshint strict: false */
+/* globals $, c3 */
 $(function() {
   $('.savePNG').on('click',function(e){
-    e.preventDefault;
+    e.preventDefault();
     createChartImages();
   });
-   var chart = c3.generate({
+   window.chart = c3.generate({
         data: {
+            x: 'x',
             columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]]
-        },point: {
-        show: false
-    }
+              ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+              ['data1', 30, 200, 100, 400, 150, 250]
+            ]
+        },
+        point: {
+            show: false
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m-%d'
+                }
+            }
+        }
+
     });
 
    var styles;
@@ -94,5 +107,5 @@ $(function() {
        }
        return output;
    };
-   
+
 });
